ca273fc4449c377175b5a45bd2a94604d56eb67e
Flore <flore@mozfr.org>
2015-02-03 00:11:45 +0100

    Wrong theme uploaded last time

--- blog/wp-content/themes/womoz_theme/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
-	<div>
-		<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-		<input type="submit" id="searchsubmit" value="Ok" />
-	</div>
-</form>
